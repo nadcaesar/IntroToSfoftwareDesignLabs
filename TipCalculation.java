51837a8850247e0abd339e16c6cf4a4975e54bca
Nick Caesar <93226878+nadcaesar@users.noreply.github.com>
2022-11-16 20:27:47 -0500

    Add files via upload

---
+++ TipCalculation.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main;
+/*
+  Name: Nicholas Caesar
+  Date: 4/15/2020
+  Description: Asks the user to enter their first name, last name, first number, and second number, and then returns the user first and last name and the result of the calculations based on the numbers the user chose
+*/
+
+public class TipCalculation 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println();
+        
+    }
+    
+}
